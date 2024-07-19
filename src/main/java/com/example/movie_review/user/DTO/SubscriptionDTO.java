841b499a6cfbd452eb8f3c150a270b2ee7d7d47f
daehui <joedaehui1@naver.com>
2024-07-19 23:21:28 +0900

    After Subscription Subscriber success

--- src/main/java/com/example/movie_review/user/DTO/SubscriptionDTO.java
+++ src/main/java/com/example/movie_review/user/DTO/SubscriptionDTO.java
@@ -3,11 +3,26 @@ package com.example.movie_review.user.DTO;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Data
 @Builder
-public class SubscriptionDTO {
+public class SubscriptionDTO implements UserActivityDTO{
     private UserCommonDTO userCommonDTO;
     private List<UserCommonDTO> subscriptionDTOs;
+
+    @Override
+    public List<?> getActivityItems() {
+        return subscriptionDTOs;
+    }
+
+    @Override
+    public Map<String, Object> toMap() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("userCommonDTO", userCommonDTO);
+        map.put("subscriptionDTOs", subscriptionDTOs);
+        return map;
+    }
 }
---
+++ src/main/java/com/example/movie_review/user/service/SubscriberService.java
@@ -0,0 +1,34 @@
+package com.example.movie_review.user.service;
+
+import com.example.movie_review.user.DTO.SubscriptionDTO;
+import com.example.movie_review.user.DTO.UserActivityDTO;
+import com.example.movie_review.user.DTO.UserActivityDTOAdapter;
+import com.example.movie_review.user.DTO.UserDTOService;
+import com.example.movie_review.user.SortOption;
+import com.example.movie_review.user.UserActivityService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service("subscriber")
+@RequiredArgsConstructor
+public class SubscriberService implements UserActivityService {
+    private final UserDTOService userDTOService;
+
+    @Override
+    public UserActivityDTO getUserActivity(String userEmail, String sort, int page, int size) {
+        SubscriptionDTO dto = userDTOService.getUserSubscribersDTO(userEmail);
+        return new UserActivityDTOAdapter(dto);
+    }
+
+    @Override
+    public List<SortOption> getSortOptions() {
+        return Arrays.asList(
+                new SortOption("favorite_date_desc", "구독자 최근"),
+                new SortOption("favorite_date_asc", "구독자 과거"),
+                new SortOption("release_date_desc", "개봉일 최신순"),
+                new SortOption("release_date_asc", "개봉일 과거순")
+        );    }
+}
