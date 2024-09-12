e97afae2b80b40139d19a42a281032342e4d35bc
daehui <joedaehui1@naver.com>
2024-09-12 20:13:22 +0900

    modify scripts

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 cd /home/ubuntu/movie_review
+
 chown -R ubuntu:ubuntu /home/ubuntu/movie_review
 chmod -R 755 /home/ubuntu/movie_review
 chown -R ubuntu:ubuntu /home/ubuntu/.gradle
 chmod -R 755 /home/ubuntu/.gradle
+
+chmod +x ./gradlew
+./gradlew clean
 ./gradlew build
+
+chown ubuntu:ubuntu build/libs/movie_review-0.0.1-SNAPSHOT.jar
+chmod 755 build/libs/movie_review-0.0.1-SNAPSHOT.jar
