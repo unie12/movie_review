0950e56b6917d6331210bc7e0c17ce6a2b01109c
daehui <joedaehui1@naver.com>
2024-09-12 21:10:55 +0900

    Modify appspec yml + sh file

--- scripts/stop_application.sh
+++ scripts/stop_application.sh
@@ -1,6 +1,16 @@
+##!/bin/bash
+#if systemctl is-active --quiet movie_review; then
+#  sudo systemctl stop movie_review
+#else
+#  pkill -f 'java -jar.*movie_review-0.0.1-SNAPSHOT.jar'
+#fi
+
 #!/bin/bash
-if systemctl is-active --quiet movie_review; then
-  sudo systemctl stop movie_review
-else
-  pkill -f 'java -jar.*movie_review-0.0.1-SNAPSHOT.jar'
+OLD_PID=$(pgrep -f 'java -jar.*movie_review-0.0.1-SNAPSHOT.jar')
+if [ ! -z "$OLD_PID" ]; then
+  kill $OLD_PID
+  sleep 10
+  if ps -p $OLD_PID > /dev/null; then
+    kill -9 $OLD_PID
+  fi
 fi
