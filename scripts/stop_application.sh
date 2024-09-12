0d4579b26b776b543368f37d7927470cd24c656b
daehui <joedaehui1@naver.com>
2024-09-12 20:17:02 +0900

    modify scripts

--- scripts/stop_application.sh
+++ scripts/stop_application.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if systemctl is-active --quiet movie-review; then
-  sudo systemctl stop movie-review
+if systemctl is-active --quiet movie_review; then
+  sudo systemctl stop movie_review
 else
   pkill -f 'java -jar.*movie_review-0.0.1-SNAPSHOT.jar'
 fi
