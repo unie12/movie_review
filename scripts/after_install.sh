6c9a6b2b730b17f22b1b292038d19adc46c76192
daehui <joedaehui1@naver.com>
2024-09-12 20:09:10 +0900

    modify scripts

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 cd /home/ubuntu/movie_review
-sudo chown -R ubuntu:ubuntu .
-sudo chmod -R 755 .
-sudo find . -type f -exec chmod 644 {} \;
-sudo find . -type d -exec chmod 755 {} \;
-sudo chmod -R 777 .gradle
+chown -R ubuntu:ubuntu /home/ubuntu/movie_review
+chmod -R 755 /home/ubuntu/movie_review
+chown -R ubuntu:ubuntu /home/ubuntu/.gradle
+chmod -R 755 /home/ubuntu/.gradle
 ./gradlew build
