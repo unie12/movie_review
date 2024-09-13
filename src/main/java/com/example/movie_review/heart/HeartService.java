e2dfbb2002ba7ebb3bb5c14901e8fdb97a17ddea
daehui <joedaehui1@naver.com>
2024-09-13 15:00:43 +0900

    EntityManager heartcount update

--- src/main/java/com/example/movie_review/heart/HeartService.java
+++ src/main/java/com/example/movie_review/heart/HeartService.java
@@ -5,6 +5,7 @@ import com.example.movie_review.review.event.ReviewEvent;
 import com.example.movie_review.review.service.ReviewService;
 import com.example.movie_review.user.domain.User;
 import com.example.movie_review.user.service.UserService;
+import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
@@ -24,6 +25,7 @@ public class HeartService {
     private final ReviewService reviewService;
 
     private final ApplicationEventPublisher eventPublisher;
+    private final EntityManager entityManager;
 
     @Transactional(isolation = Isolation.READ_COMMITTED)
     public boolean toggleHeart(String email, Long reviewId, boolean isHeart) {
@@ -37,23 +39,36 @@ public class HeartService {
             Heart heart = new Heart();
             heart.setUser(user);
             heart.setReview(review);
-            heartRepository.save(heart);
+            saveHeart(heart);
             result = true;
         }
         // 좋아요 눌려 있던 상태
         else if (!isHeart && existingHeart.isPresent()){
-            heartRepository.delete(existingHeart.get());
+            deleteHeart(existingHeart.get());
             result = false;
         } else {
             result = existingHeart.isPresent();
         }
 
-        reviewService.saveReview(review);
+        entityManager.flush();
+        entityManager.refresh(review);
+
+//        reviewService.saveReview(review);
 
         Review updatedReview = reviewService.getReviewById(reviewId);
         eventPublisher.publishEvent(new ReviewEvent(this, updatedReview, ReviewEvent.ReviewEventType.HEART));
 
         return result;
+    }
+
+    @Transactional
+    public void saveHeart(Heart heart) {
+        heartRepository.save(heart);
+    }
+
+    @Transactional
+    public void deleteHeart(Heart heart) {
+        heartRepository.delete(heart);
     }
 
     public List<Long> getUserHearts(String email) {
