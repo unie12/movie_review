788b9d63d123cef0bb2917650e7ab5c89c0cfdcd
daehui <joedaehui1@naver.com>
2024-09-22 13:19:03 +0900

    Modify codeDeploy operation

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-cd /home/ubuntu/movie_review
-nohup java -jar build/libs/movie_review-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
+sudo systemctl start ajoukino.service
--- scripts/stop_application.sh
+++ scripts/stop_application.sh
@@ -1,16 +1,7 @@
-##!/bin/bash
-#if systemctl is-active --quiet movie_review; then
-#  sudo systemctl stop movie_review
-#else
-#  pkill -f 'java -jar.*movie_review-0.0.1-SNAPSHOT.jar'
-#fi
-
 #!/bin/bash
-OLD_PID=$(pgrep -f 'java -jar.*movie_review-0.0.1-SNAPSHOT.jar')
-if [ ! -z "$OLD_PID" ]; then
-  kill $OLD_PID
-  sleep 10
-  if ps -p $OLD_PID > /dev/null; then
-    kill -9 $OLD_PID
-  fi
+
+sudo systemctl stop ajoukino.service
+sleep 10
+if systemctl is-active --quiet ajoukino.service; then
+  sudo systemctl kill ajoukino.service
 fi
