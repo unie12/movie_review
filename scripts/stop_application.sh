45c86503d6ba59216cf63a4e37452881d10accbd
daehui <joedaehui1@naver.com>
2024-09-22 13:57:36 +0900

    Modify codeDeploy operation

--- scripts/stop_application.sh
+++ scripts/stop_application.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Find the PID of the Java process
-PID=$(pgrep -f "java.*movie_review-0.0.1-SNAPSHOT.jar")
+PID=$(sudo pgrep -f "java.*movie_review-0.0.1-SNAPSHOT.jar")
 
 if [ -n "$PID" ]; then
   echo "Stopping Java process with PID: $PID"
   sudo kill $PID
   sleep 10
 
-  if ps -p $PID > /dev/null; then
+  if sudo ps -p $PID > /dev/null; then
     echo "Process did not stop gracefully. Forcing termination."
     sudo kill -9 $PID
   fi
